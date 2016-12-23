0821e4b82c801123f1e9370826f46a308e40c0e6
Jacob Martin <mindnumb@gmail.com>
2016-12-22 16:42:40 -0800

    Update database engine/bindings

--- proto/settings.py
+++ proto/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'proto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'proto',
     }
 }
