94607dfacd819612b4ffa402af429e9642a4f3fd
Jacob Martin <mindnumb@gmail.com>
2017-01-01 23:31:04 -0700

    Model strings

--- search/models.py
+++ search/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Story(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     contents = models.TextField
+
+    def __str__(self):
+        return self.title
