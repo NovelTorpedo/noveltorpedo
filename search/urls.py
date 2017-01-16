7a346e6ba27cd88a50094f696fa855729586e91e
Jacob Martin <mindnumb@gmail.com>
2017-01-15 17:53:25 -0800

    Massaging form into place

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'search'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- search/views.py
+++ search/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Welcome.</h1>");
+    return render(request, 'search/pages/home.html')
+
+def search(request):
+    return HttpResponse('Search received')
