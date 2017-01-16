bc8081bf098bf8d8f9323033311074021b39dd81
Jacob Martin <mindnumb@gmail.com>
2017-01-15 18:20:00 -0800

    Brook ftw

--- search/views.py
+++ search/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'search/pages/home.html')
+
 
 def search(request):
     return HttpResponse('Search received')
