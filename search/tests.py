ad9dbced009406e4cee52191fc4bfc32e8a6637b
Jacob Martin <mindnumb@gmail.com>
2017-01-17 18:27:32 -0800

    Basic initial test

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+client = Client()
+
+
+class SearchTests(TestCase):
+
+    def test_that_the_front_page_loads_properly(self):
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'NovelTorpedo Search')
