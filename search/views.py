d9a6a1f3cc4d5ee7fe669447fb9446c54b585660
Jacob Martin <mindnumb@gmail.com>
2017-01-17 19:57:23 -0800

    Import bootstrap from CDN; refactor templates

--- search/views.py
+++ search/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
     return render(request, 'search/pages/home.html')
 
 
 def search(request):
-    return HttpResponse('Search received')
+    return HttpResponse(request.POST['search'])
