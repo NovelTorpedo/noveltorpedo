a697ac8cb9b5bde33798988a714e4147a8d2fa05
Jacob Martin <mindnumb@gmail.com>
2017-01-19 19:33:39 -0800

    Initial story index

---
+++ noveltorpedo/search_indexes.py
@@ -0,0 +1,10 @@
+from haystack import indexes
+from noveltorpedo.models import Story
+
+
+class Story(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title')
+
+    def get_model(self):
+        return Story
