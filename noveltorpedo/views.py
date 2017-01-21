c3670110d4235f2021fc8a50c5695a4b9621b4db
Jacob Martin <mindnumb@gmail.com>
2017-01-21 13:52:13 -0800

    Clean up a bit

--- noveltorpedo/views.py
+++ noveltorpedo/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def home(request):
-    return render(request, 'noveltorpedo/pages/home.html')
-
-
-def search(request):
-    return HttpResponse(request.GET['search'])
+# Views go here
