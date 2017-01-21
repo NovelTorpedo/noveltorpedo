de668970017d5568a57bd12b59a4aa0c7be48d1a
Jacob Martin <mindnumb@gmail.com>
2017-01-21 14:21:26 -0800

    Typo

--- noveltorpedo/forms.py
+++ noveltorpedo/forms.py
@@ -3,4 +3,4 @@ from haystack.forms import SearchForm as HaystackSearchForm
 
 class SearchForm(HaystackSearchForm):
     def __init__(self, *args, **kwargs):
-        super(SearchForm).__init(self, *args, **kwargs)
+        super(SearchForm, self).__init__(*args, **kwargs)
