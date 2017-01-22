83ef9579dd639ecf088f220bec2c941f32422c74
Jacob Martin <mindnumb@gmail.com>
2017-01-22 12:05:18 -0800

    Static files

--- proto/settings.py
+++ proto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&m+-8z0js-5kz9-9-u%jdzx+rwvl!$+1q+ube#@ua9x+40ixr8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cs470-litrocket.cs.pdx.edu"]
 
 
 # Application definition
@@ -134,3 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
