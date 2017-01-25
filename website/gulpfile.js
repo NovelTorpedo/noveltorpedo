753a255c990ebd574c1f5cc1892a606d1500a018
Jacob Martin <mindnumb@gmail.com>
2017-01-24 18:14:04 -0800

    Compile sass with gulp

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
-gulp.task('default', function() {
+// Compile all our JavaScript.
+gulp.task('js', function() {
     return gulp.src('resources/js/main.js')
-        .pipe(webpack())
-        .pipe(gulp.dest('static/js.js'));
+        .pipe(webpack({
+            output: {
+                filename: 'app.js'
+            }
+        }))
+        .pipe(gulp.dest('static/'));
 });
+
+// Compile all of our Sass.
+gulp.task('scss', function() {
+    return gulp.src('resources/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('static/'));
+});
+
+gulp.task('default', ['js', 'scss']);
