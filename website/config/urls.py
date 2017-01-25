498a1af2390767ab2998932735c9cb58b0012eb6
Jacob Martin <mindnumb@gmail.com>
2017-01-24 17:36:55 -0800

    Rename "proto" -> "config"

--- website/config/urls.py
+++ website/config/urls.py
@@ -1,4 +1,5 @@
-"""proto URL Configuration
+"""
+NovelTorpedo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- website/config/wsgi.py
+++ website/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for proto project.
+WSGI config for NovelTorpedo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proto.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
