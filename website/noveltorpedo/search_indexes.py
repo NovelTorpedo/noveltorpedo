9a48cb325d4755955c7969fb273fa7ec623dab5a
Jacob Martin <mindnumb@gmail.com>
2017-01-29 15:07:03 -0800

    Update story indexes

--- website/noveltorpedo/search_indexes.py
+++ website/noveltorpedo/search_indexes.py
@@ -4,8 +4,6 @@ from noveltorpedo.models import Story
 
 class StoryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    contents = indexes.CharField(model_attr='contents')
 
     def get_model(self):
         return Story
