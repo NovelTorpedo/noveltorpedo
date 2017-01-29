76f113e0086586d085b0ea988d799f53f55bf87e
Jacob Martin <mindnumb@gmail.com>
2017-01-29 12:40:51 -0800

    Updating models

--- website/noveltorpedo/models.py
+++ website/noveltorpedo/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Host(models.Model):
+    baseurl = models.CharField(max_length=511)
+    spider = models.CharField(max_length=255)
+    wait = models.IntegerField()
+
+    def __str__(self):
+        return self.baseurl
+
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
@@ -8,8 +17,19 @@ class Author(models.Model):
         return self.name
 
 
+class StoryHosts(models.Model):
+    host_id = models.IntegerField()
+    story_id = models.IntegerField()
+    story_url = models.CharField(max_length=511)
+    last_scraped = models.DateTimeField()
+
+    class Meta:
+        db_table = 'story_hosts'
+
+
 class Story(models.Model):
     authors = models.ManyToManyField(Author)
+    hosts = models.ManyToManyField(Host, through=StoryHosts)
     title = models.CharField(max_length=255)
     contents = models.TextField(default='')
 
@@ -21,6 +41,9 @@ class StoryAttribute(models.Model):
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
     key = models.CharField(max_length=255)
     value = models.CharField(max_length=511)
+
+    class Meta:
+        db_table = 'story_attributes'
 
     def __str__(self):
         return self.story.__str__() + ' - ' + self.key
