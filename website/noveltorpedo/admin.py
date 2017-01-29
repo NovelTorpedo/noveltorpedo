2d6fed487119a637e5678415647211b37fc2b987
Jacob Martin <mindnumb@gmail.com>
2017-01-29 13:04:51 -0800

    Update admin model registrations

--- website/noveltorpedo/admin.py
+++ website/noveltorpedo/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from noveltorpedo.models import *
 
+admin.site.register(Host)
 admin.site.register(Author)
+admin.site.register(AuthorAttribute)
 admin.site.register(Story)
 admin.site.register(StoryAttribute)
+admin.site.register(StorySegment)
