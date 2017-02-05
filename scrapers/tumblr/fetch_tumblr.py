8387c6fe3886b86e14a566d02fe0fe87452a9a79
Finn Ellis <relsqui@chiliahedron.com>
2017-02-04 16:44:02 -0800

    Add Tumblr story info to database.

--- scrapers/tumblr/fetch_tumblr.py
+++ scrapers/tumblr/fetch_tumblr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from datetime import datetime, MINYEAR
 import pytumblr
 from secrets import consumer_key, secret_key, oauth_token, oauth_secret
 
@@ -15,12 +16,9 @@ django.setup()
 
 from noveltorpedo import models
 
-client = pytumblr.TumblrRestClient(
-        consumer_key,
-        secret_key,
-        oauth_token,
-        oauth_secret
-)
+# These globals will be initialized later.
+client = None   # Tumblr API client
+host = None     # Tumblr Host object in the database
 
 def get_info(blog):
     """
@@ -50,11 +48,15 @@ def get_posts(blog, offset=0, limit=20):
     return client.posts(blog, type="text", filter="text",
                         offset=offset, limit=limit)["posts"]
 
-def create_host():
+def get_host():
     """
     Create the Tumblr host entry in the database iff it doesn't exist.
+    In either case, returns the appropriate Host object, as well as saving
+    it in a global variable so we don't have to query for it again.
     """
-    host = models.Host.objects.filter(url = "tumblr.com")
+    global host
+    if not host:
+        host = models.Host.objects.filter(url = "tumblr.com").first()
     if not host:
         host = models.Host()
         host.url = "tumblr.com"
@@ -63,15 +65,37 @@ def create_host():
         host.save()
     return host
 
+def make_story(blog):
+    """
+    Add a new story to the database. Takes a Tumblr blog name and returns
+    the corresponding Story object. Does no verification that the story
+    hasn't already been added, and returns nothing.
+    """
+    blog_info = client.blog_info(blog)["blog"]
+    story = models.Story()
+    story.title = blog_info["title"]
+    story.save()
+
+    author = models.Author()
+    author.name = blog
+    author.save()
+    story.authors.add(author)
+
+    storyhost = models.StoryHost()
+    storyhost.host = get_host()
+    storyhost.url = blog + ".tumblr.com"
+    storyhost.story = story
+    storyhost.last_scraped = datetime(MINYEAR, 1, 1)
+    storyhost.save()
+
 if __name__ == "__main__":
     """
     This allows you to pass the short name of a tumblr on the command line to
-    verify that its information can be retrieved from the API. Examples:
-        antlerscolorado
-        docfuture
-    Also creates the Tumblr host entry in the database if it doesn't exist.
+    add it as a new entry to the story database. Doesn't retrieve posts yet.
     """
     import sys
+    client = pytumblr.TumblrRestClient(consumer_key, secret_key, oauth_token,
+                                       oauth_secret)
     try:
         blog = sys.argv[1]
         info = get_info(blog)
@@ -83,4 +107,4 @@ if __name__ == "__main__":
         print("Tumblr not found.")
         sys.exit(1)
 
-    create_host()
+    make_story(blog)
