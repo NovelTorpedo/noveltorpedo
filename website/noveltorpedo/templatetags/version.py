3929b0951957f2e8c8e6b1a841c9950d058025eb
Jacob Martin <mindnumb@gmail.com>
2017-02-10 20:40:17 -0800

    Editing versioning tag so it actually works

--- website/noveltorpedo/templatetags/version.py
+++ website/noveltorpedo/templatetags/version.py
@@ -1,10 +1,10 @@
 from django import template
-import os
-import re
 from config.settings import STATICFILES_DIRS
+import os
+# import re
 
 """
-Thanks to:
+Adapted from:
 
 https://muffinresearch.co.uk/automatic-asset-versioning-in-django/
 """
@@ -14,7 +14,7 @@ register = template.Library()
 STATIC_PATH = STATICFILES_DIRS[0]
 version_cache = {}
 
-rx = re.compile(r"^(.*)\.(.*?)$")
+# rx = re.compile(r"^(.*)\.(.*?)$")
 
 
 def version(path_string):
@@ -22,11 +22,12 @@ def version(path_string):
         if path_string in version_cache:
             mtime = version_cache[path_string]
         else:
-            mtime = os.path.getmtime('%s%s' % (STATIC_PATH, path_string,))
+            mtime = os.path.getmtime(os.path.join(STATIC_PATH, path_string))
             version_cache[path_string] = mtime
 
-        return rx.sub(r"\1.%d.\2" % mtime, path_string)
+        # return rx.sub(r"\1.%d.\2" % mtime, path_string)
+        return '/static/' + path_string + "?v=" + str(int(mtime))
     except:
-        return path_string
+        return '/static/' + path_string
 
 register.simple_tag(version)
