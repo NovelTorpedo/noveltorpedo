6dac4b5a32938f709777cfaa9d9e2dedd287cc4f
Jacob Martin <mindnumb@gmail.com>
2017-03-02 12:05:09 -0800

    Check for highlighted words

--- website/noveltorpedo/tests.py
+++ website/noveltorpedo/tests.py
@@ -30,24 +30,28 @@ class SearchTests(TestCase):
         segment.published = timezone.now()
         segment.story = story
         segment.title = 'Chapter Three'
-        segment.contents = 'This is how it all went down...'
+        segment.contents = 'Righteous justice was distributed...'
         segment.save()
 
         # Index the new story.
         call_command('update_index')
 
-        def check_response(response):
+        def check_response(response, highlighted_words=[]):
             self.assertEqual(response.status_code, 200)
             self.assertContains(response, 'Jack Frost')
             self.assertContains(response, 'The Big One')
-            self.assertContains(response, 'Chapter Three')
-            self.assertContains(response, 'This is how it all went down...')
+
+            for word in highlighted_words:
+                self.assertContains(response, '<span class="highlighted">' + word + '</span>')
 
         # Query via author name.
         check_response(client.get('/', {'q': 'Jack Frost'}))
 
-        # Query via story name.
+        # Query via story title.
         check_response(client.get('/', {'q': 'The Big One'}))
 
+        # Query via segment title.
+        check_response(client.get('/', {'q': 'Chapter Three'}), ['Chapter', 'Three'])
+
         # Query via segment contents.
-        check_response(client.get('/', {'q': 'Chapter Three'}))
+        check_response(client.get('/', {'q': 'Righteous justice'}, ['Righteous', 'justice']))
