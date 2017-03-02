543fc894120db6e8d854e746d631c87cc53f622b
Jacob Martin <mindnumb@gmail.com>
2017-03-02 11:45:28 -0800

    Rebuild index and test variety of queries

--- website/noveltorpedo/tests.py
+++ website/noveltorpedo/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.test import Client
 from noveltorpedo.models import *
-import unittest
 from django.utils import timezone
+from django.core.management import call_command
 
 client = Client()
 
@@ -15,12 +15,13 @@ class SearchTests(TestCase):
         self.assertContains(response, 'NovelTorpedo Search')
 
     def test_insertion_and_querying_of_data(self):
+        # Create a new story in the database.
         author = Author()
-        author.name = "Jack Frost"
+        author.name = 'Jack Frost'
         author.save()
 
         story = Story()
-        story.title = "The Big One"
+        story.title = 'The Big One'
         story.save()
 
         story.authors.add(author)
@@ -28,6 +29,33 @@ class SearchTests(TestCase):
         segment = StorySegment()
         segment.published = timezone.now()
         segment.story = story
-        segment.title = "Chapter One"
-        segment.contents = "This is how it all went down..."
+        segment.title = 'Chapter Three'
+        segment.contents = 'This is how it all went down...'
         segment.save()
+
+        # Index the new story.
+        call_command('update_index')
+
+        # Query via author name.
+        response = client.get('/', {'q': 'Jack Frost'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Jack Frost')
+        self.assertContains(response, 'The Big One')
+        self.assertContains(response, 'Chapter Three')
+        self.assertContains(response, 'This is how it all went down...')
+
+        # Query via story name.
+        response = client.get('/', {'q': 'The Big One'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Jack Frost')
+        self.assertContains(response, 'The Big One')
+        self.assertContains(response, 'Chapter Three')
+        self.assertContains(response, 'This is how it all went down...')
+
+        # Query via segment contents.
+        response = client.get('/', {'q': 'Chapter Three'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Jack Frost')
+        self.assertContains(response, 'The Big One')
+        self.assertContains(response, 'Chapter Three')
+        self.assertContains(response, 'This is how it all went down...')
