2471d773c6a463e73ae4e6b0c78134313398fa12
Sage Callon <scallon@pdx.edu>
2017-03-02 15:03:59 -0800

    Added registry page; does nothing at this time.

--- website/noveltorpedo/views.py
+++ website/noveltorpedo/views.py
@@ -4,6 +4,10 @@ from noveltorpedo.forms import SearchForm
 from haystack.views import SearchView as HaystackSearchView
 
 
+def register(request):
+    return HttpResponse("Registration coming soon(tm).")
+
+
 class SearchView(HaystackSearchView):
     def __init__(self, template=None, load_all=True, searchqueryset=None):
         self.form_class = SearchForm
