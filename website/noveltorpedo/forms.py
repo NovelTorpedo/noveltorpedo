0f8998d79d141a28e0c675190107306651154658
Rubin Stricklin <rubin2@pdx.edu>
2017-03-05 11:18:52 -0800

    added email requirement to user

--- website/noveltorpedo/forms.py
+++ website/noveltorpedo/forms.py
@@ -1,5 +1,7 @@
+from django import forms
 from haystack.forms import SearchForm as HaystackSearchForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class SearchForm(HaystackSearchForm):
@@ -10,3 +12,15 @@ class SearchForm(HaystackSearchForm):
 class RegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
+
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
