ccd6f5189f71cb6eaa18eac8cb59c6a209940eda
Matthew Popescu <Matthew Popescu>
2017-03-07 17:54:19 -0800

    Slight mod to (sb) test run script

--- scrapers/Spacebattles/runtests.bat
+++ scrapers/Spacebattles/runtests.bat
@@ -1,4 +1,5 @@
 @echo off
+call python reset_db.py
 call scrapy crawl sb_spider -s HTTPCACHE_ENABLED=1 -s HTTPCACHE_EXPIRATION_SECS=0 ^
                             -s HTTPCACHE_STORAGE="scrapy.extensions.httpcache.FilesystemCacheStorage" ^
                             -s HTTPCACHE_IGNORE_MISSING=1 ^
