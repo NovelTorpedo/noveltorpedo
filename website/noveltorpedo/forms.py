cd110cd696f9def7b699c96fad094e5105c0f0ec
Jacob Martin <mindnumb@gmail.com>
2017-03-10 15:28:40 -0800

    Simplifying login

--- website/noveltorpedo/forms.py
+++ website/noveltorpedo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from haystack.forms import SearchForm as HaystackSearchForm
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import BaseForm
 from noveltorpedo.models import StoryHost
@@ -8,11 +8,6 @@ import requests
 
 
 class SearchForm(HaystackSearchForm):
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-
-
-class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
 
