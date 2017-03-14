1477ceec5a6ee7a3fd00343506b4dfcf8379991c
Brook Boese <xbrookbx@gmail.com>
2017-03-13 20:38:54 -0700

    Errors from noveltorpedo/forms.py for submitting a story:
    importing noveltorpedo.fetch_tumblr.
    error from forms/forms.py BaseForm class regarding variable base_fields (line 139).

--- website/noveltorpedo/forms.py
+++ website/noveltorpedo/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import BaseForm
 from noveltorpedo.models import StoryHost
-from noveltorpedo.fetch_tumblr import get_or_create_storyhost
+#from noveltorpedo.fetch_tumblr import get_or_create_storyhost
 import requests
 
 
@@ -32,6 +32,8 @@ class RegistrationForm(UserCreationForm):
 
 
 class TumblrAddForm(BaseForm):
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
 
     name = forms.CharField(required=True, label='Tumblr Username')
 
--- website/noveltorpedo/urls.py
+++ website/noveltorpedo/urls.py
@@ -6,5 +6,5 @@ app_name = 'noveltorpedo'
 urlpatterns = [
     url(r'^$', views.SearchView(), name='haystack_search'),
     url(r'^register', views.register, name='register'),
-    url(r'^submit', views.submit_story_tumblr(), name='submit_story_tumblr')
+    url(r'^submit', views.submit_story_tumblr, name='submit_story_tumblr')
 ]
