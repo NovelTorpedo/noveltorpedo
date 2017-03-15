6c1ea48e825358536e3ed410ee9974fc54f57fcb
Brook Boese <xbrookbx@gmail.com>
2017-03-14 18:30:41 -0700

    Trouble with git

--- website/noveltorpedo/forms.py
+++ website/noveltorpedo/forms.py
@@ -15,8 +15,6 @@ class SearchForm(HaystackSearchForm):
 
 
 class RegistrationForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
 
     email = forms.EmailField(required=True)
 
@@ -33,14 +31,12 @@ class RegistrationForm(UserCreationForm):
 
 
 class TumblrAddForm(Form):
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
 
     name = forms.CharField(required=True, label='Tumblr Username')
 
     def clean_name(self):
         name = self.cleaned_data.get("name")
-        url = name + '.tumblr.com'
+        url = 'http://' + name + '.tumblr.com'
 
         if StoryHost.objects.filter(url=url).count():
             raise forms.ValidationError('We\'re already tracking stories from ' + name + ' on Tumblr.')
