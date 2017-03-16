1ca7352fc89c8d09e0b9a5ac80143899b71d9bac
Matthew Popescu <Matthew Popescu>
2017-03-16 12:45:32 -0700

    added time wait to spider_manager (sb)

--- scrapers/Spacebattles/spider_manager.py
+++ scrapers/Spacebattles/spider_manager.py
@@ -13,34 +13,49 @@
 # IN THE SOFTWARE.
 
 from scrapy.crawler import CrawlerRunner
+from datetime import datetime
 import time
-import scrapy
+from datetime import timedelta
+
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor
 import sys
 import os
 import django
-from django.test import TestCase
-from tests.reset_db import reset_database
 
 sys.path.insert(0, "../../website")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 
-def continue_crawl(d, runner):
+def continue_crawl(d, runner, begin_time, hour_limit):
     """
     Continues the crawler process.
 
     :param d:
     :param runner:
+    :param begin_time
+    :param hour_limit
     :return:
     """
-    # maybe put a time.sleep call here?
+    end_time = datetime.now()
+
+    # Convert elapsed time to seconds
+    elapsed_time = end_time - begin_time
+    elapsed_time_seconds = elapsed_time.days * 86400
+    elapsed_time_seconds += elapsed_time.seconds
+
+    print("Total time elapsed: {0}".format(elapsed_time_seconds))
+    if elapsed_time_seconds < hour_limit:
+        wait_time = hour_limit - elapsed_time_seconds
+        print("Waiting for {0} seconds".format(wait_time))
+        time.sleep(wait_time)
 
     d = runner.crawl("sb_spider")
 
-    d.addBoth(lambda _: continue_crawl(d, runner))
+    # end_time is the new "start" time for this iteration of the spider.
+    # Pass it in as the 3rd argument.
+    d.addBoth(lambda _: continue_crawl(d, runner, end_time, hour_limit))
 
 def run_spiders():
     """
@@ -49,18 +64,21 @@ def run_spiders():
 
     :return:
     """
+    hour_limit = 3600  # in seconds
+
     settings = get_project_settings()
-    """
+
     settings["HTTPCACHE_ENABLED"] = 1
     settings["HTTPCACHE_EXPIRATION_SECS"] = 0
     settings["HTTPCACHE_STORAGE"] = "scrapy.extensions.httpcache.FilesystemCacheStorage"
     settings["HTTPCACHE_IGNORE_MISSING"] = 1
     settings["HTTPCACHE_DIR"] = "onepagetest"
-    """
+
     runner = CrawlerRunner(settings)
+    begin_time = datetime.now()
 
     d = runner.crawl("sb_spider")
-    d.addBoth(lambda _: continue_crawl(d, runner))
+    d.addBoth(lambda _: continue_crawl(d, runner, begin_time, hour_limit))
     reactor.run()
 
 if __name__ == "__main__":
